b836226beafbabc3eb4d4d021eb4c02dd00fe25a
ORDEREDED <84131813+ORDEREDED@users.noreply.github.com>
2022-03-31 19:46:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload(){
+
+}
+function setup(){
+canvas=createCanvas(280, 280);
+canvas.center();
+background("red");
+}
+function draw(){
+
+}
+function clearu(){
+background("white");
+}
